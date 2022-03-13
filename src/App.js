43d03997832b33f589301f4cf6f65e1d0c7eb858
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-03-13 20:51:35 +0700

    import scss

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import "./views/Navigation/Nav";
+import Nav from "./views/Navigation/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Welcome to react-hooks</h3>
       </header>
     </div>
   );
