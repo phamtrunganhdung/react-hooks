99e12e805bc28ef30c8e675b864283b2381cfd90
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-03-21 18:34:39 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import logo from "./logo.svg";
 import "./App.scss";
 import "./views/Navigation/Nav";
 import Nav from "./views/Navigation/Nav";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  let [title, setTitle] = useState("Hello world");
+  const [titleInput, setTitleInput] = useState("");
+  const handleSetTile = () => {
+    setTitle(titleInput);
+    setTitleInput("");
+  };
+  const handleInputTitle = (event) => {
+    setTitleInput(event.target.value);
+  };
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to react-hooks</h3>
+        <h3>{title}</h3>
+
+        <input
+          type="text"
+          value={titleInput}
+          onChange={(e) => handleInputTitle(e)}
+          placeholder="enter title"
+        />
+        <br />
+        <button onClick={() => handleSetTile()}>Set Title</button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
