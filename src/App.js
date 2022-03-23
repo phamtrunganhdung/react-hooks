c03649778f30f83252e1c8bff137a62a76182dff
Phạm Trung Anh Dũng <dungpham4320@gmail.com>
2022-03-23 18:30:05 +0700

    Outputing List

--- src/App.js
+++ src/App.js
@@ -5,30 +5,60 @@ import Nav from "./views/Navigation/Nav";
 import { useState } from "react";
 
 const App = () => {
-  let [title, setTitle] = useState("Hello world");
-  const [titleInput, setTitleInput] = useState("");
-  const handleSetTile = () => {
-    setTitle(titleInput);
-    setTitleInput("");
+  let [todos, setTodos] = useState([
+    {
+      id: "todo1",
+      title: "Reading book",
+    },
+    {
+      id: "todo2",
+      title: "Playing game",
+    },
+    {
+      id: "todo3",
+      title: "Playing soccer",
+    },
+  ]);
+  const [inputAddTodo, setInputAddTodo] = useState("");
+  const handleAddTodo = () => {
+    let newTodo = {
+      id: "todo" + Math.floor(Math.random() * 10000),
+      title: inputAddTodo,
+    };
+
+    if (!inputAddTodo) {
+      alert("empty data");
+      return;
+    }
+    setTodos([...todos, newTodo]);
+    setInputAddTodo("");
   };
-  const handleInputTitle = (event) => {
-    setTitleInput(event.target.value);
+  const handleOnChangeInputAddTodo = (event) => {
+    setInputAddTodo(event.target.value);
   };
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>{title}</h3>
-
+        <h3>React Hooks</h3>
+        <div className="todo-container">
+          {todos.map((todo) => {
+            return (
+              <li className="todo-child" key={todo.id}>
+                {todo.title}
+              </li>
+            );
+          })}
+        </div>
         <input
           type="text"
-          value={titleInput}
-          onChange={(e) => handleInputTitle(e)}
-          placeholder="enter title"
+          value={inputAddTodo}
+          onChange={(e) => handleOnChangeInputAddTodo(e)}
+          placeholder="Add new todo"
         />
         <br />
-        <button onClick={() => handleSetTile()}>Set Title</button>
+        <button onClick={() => handleAddTodo()}>Add Todo</button>
       </header>
     </div>
   );
